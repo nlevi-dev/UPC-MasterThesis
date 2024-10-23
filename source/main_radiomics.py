4aa2631e8ade62bebe34249c149f9280cb401be5
levente <nagyleventezsolt@gmail.com>
2024-10-23 10:51:02 +0200

    sync

--- source/main_radiomics.py
+++ source/main_radiomics.py
@@ -1,8 +1,13 @@
+import sys
 from DataHandler import DataHandler
 
 kernelWidth=5
 binWidth=25
-handler = DataHandler(debug=True, out='l_radiomics_voxel.log', cores=-1)
+if len(sys.argv) > 1:
+    kernelWidth=int(sys.argv[1])
+    binWidth=int(sys.argv[2])
+print('kernel_width={},bin_width={}'.format(kernelWidth,binWidth))
+handler = DataHandler(debug=True, out='main_radiomics.log', cores=-1)
 handler.radiomics(binWidth)
 handler.cores = 6
 handler.radiomicsVoxel(kernelWidth, binWidth, recompute=True)
