4326e61d1088f3d352506a6dfa4221d38d127195
Levente Nagy <nagyleventezsolt@gmail.com>
2024-10-25 16:01:05 +0200

    debug

--- source/DataGenerator.py
+++ source/DataGenerator.py
@@ -115,6 +115,7 @@ class DataGenerator():
             y = np.concatenate(y,0)
         if self.batch_size is not None:
             remainder = len(y) % self.batch_size
+            print(remainder)
             if remainder > 0:
                 y = np.concatenate([y,np.take(y,range(0,remainder),0)],0)
                 if self.type == 'CNN':
