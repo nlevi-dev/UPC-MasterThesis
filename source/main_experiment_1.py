9c7606d04d30b049f731416d7f7c8715c5c0eb48
Levente Nagy <nagyleventezsolt@gmail.com>
2024-10-25 15:04:24 +0200

    sync

--- source/main_experiment_1.py
+++ source/main_experiment_1.py
@@ -3,8 +3,8 @@ warnings.simplefilter(action='ignore',category=FutureWarning)
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Input, Conv3D, Reshape, Concatenate
 from DataGenerator import DataGenerator
 from visual import showSlices
 
@@ -16,7 +16,7 @@ props={
     'control'       : True,       #include control data points
     'huntington'    : False,      #include huntington data points
     'type'          : 'CNN',      # FNN CNN FCNN
-    'cnn_size'      : 5,          #
+    'cnn_size'      : 7,          #
     'left'          : True,       #include left hemisphere data (if both false, concatenate the left and right hemisphere layers)
     'right'         : False,      #include right hemisphere data
     'threshold'     : 0.5,        #if float value provided, it thresholds the connectivty map
@@ -40,38 +40,47 @@ if train[0][1] is not None:
     print(train[0][1].shape)
 print(train[1].shape)
 
-# model = Sequential(name='FFN')
-# model.add(Input((train[0].shape[1],)))
-# model.add(Dense(1024,activation='silu'))
-# model.add(Dense(1024,activation='silu'))
-# model.add(Dense(1024,activation='silu'))
-# model.add(Dense(512,activation='silu'))
-# model.add(Dense(512,activation='silu'))
-# model.add(Dense(512,activation='silu'))
-# model.add(Dense(256,activation='silu'))
-# model.add(Dense(256,activation='silu'))
-# model.add(Dense(256,activation='silu'))
-# model.add(Dense(128,activation='silu'))
-# model.add(Dense(128,activation='silu'))
-# model.add(Dense(128,activation='silu'))
-# model.add(Dense(64,activation='silu'))
-# model.add(Dense(64,activation='silu'))
-# model.add(Dense(64,activation='silu'))
-# model.add(Dense(train[1].shape[1],activation='softmax'))
+inputH = Input(shape=train[0][0].shape[1:],name='conv_input')
+act = 'relu'
+f0 = train[0][0].shape[-1]*2
+h = Conv3D(f0,3,strides=1,padding='valid',activation=act,name='conv_1')(inputH)
+h = Conv3D(f0,3,strides=1,padding='valid',activation=act,name='conv_2')(h)
+h = Conv3D(f0,3,strides=1,padding='valid',activation=act,name='conv_3')(h)
+h = Reshape((f0,),name='conv_flatten')(h)
+if train[0][1] is not None:
+    inputL = Input(shape=train[0][1].shape[1:],name='flat_input')
+    f1 = train[0][1].shape[-1]*2
+    l = Dense(f1,activation=act,name='flat_1')(inputL)
+    l = Dense(f1,activation=act,name='flat_2')(l)
+    l = Dense(f1,activation=act,name='flat_3')(l)
+    c = Concatenate(name='head_0')([h,l])
+    f = f0+f1
+else:
+    c = h
+    f = f0
+    train[0] = [train[0][0]]
+c = Dense(f,activation=act,name='head_1')(c)
+c = Dense(f,activation=act,name='head_2')(c)
+c = Dense(f,activation=act,name='head_3')(c)
+c = Dense(f//2,activation=act,name='head_4')(c)
+c = Dense(f//2,activation=act,name='head_5')(c)
+c = Dense(f//2,activation=act,name='head_6')(c)
+c = Dense(train[1].shape[-1],activation='softmax',name='head_output')(c)
 
-# callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 7)
+model = Model(inputs=[inputH,inputL] if len(train[0]) > 1 else [inputH], outputs=c)
 
-# model.compile(loss=tf.keras.losses.CategoricalCrossentropy, optimizer='adam', jit_compile=True)
-# history = model.fit(train[0], train[1],
-#     validation_data=val,
-#     batch_size=10000,
-#     epochs=100,
-#     verbose=1,
-#     callbacks = [callback]
-# )
+callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 7)
 
-# for t in test[0:1]:
-#     p = model.predict(t[0])
-#     showSlices(t[3],t[2](t[1]),title='original')
-#     showSlices(t[3],t[2](p),title='predicted')
+model.compile(loss=tf.keras.losses.CategoricalCrossentropy, optimizer='adam', jit_compile=True)
+history = model.fit(train[0], train[1],
+    validation_data=val,
+    batch_size=1000,
+    epochs=100,
+    verbose=1,
+    callbacks = [callback]
+)
 
+for t in test[0:1]:
+    p = model.predict(t[0])
+    showSlices(t[3],t[2](t[1]),title='original')
+    showSlices(t[3],t[2](p),title='predicted')
