5a3d56febc1b9fbeff16707a1a45755a0cb23b5d
Levente Nagy <nagyleventezsolt@gmail.com>
2024-10-31 14:26:43 +0100

    bugfix

--- source/main_radiomics.py
+++ source/main_radiomics.py
@@ -3,7 +3,7 @@ from DataHandler import DataHandler
 
 binWidth=25
 if len(sys.argv) > 1:
-    binWidth=int(sys.argv[2])
+    binWidth=int(sys.argv[1])
 print('bin_width={}'.format(binWidth))
 handler = DataHandler(debug=True, out='main_radiomics.log', cores=-1)
 handler.radiomics(binWidth, recompute=True)
