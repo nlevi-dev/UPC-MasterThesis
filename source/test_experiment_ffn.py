349d6f926df123b3fba87b0ee9c471aa70ccb70a
Levente Nagy <nagyleventezsolt@gmail.com>
2024-11-01 15:14:04 +0100

    sync

--- source/test_experiment_ffn.py
+++ source/test_experiment_ffn.py
@@ -7,4 +7,4 @@ model.load_weights('data/models/FFN.weights.h5')
 #model.summary()
 #plotModel(model)
 
-showResults(model,threshold=0,background=True)
+showResults(model,threshold=props['threshold'],background=True)
