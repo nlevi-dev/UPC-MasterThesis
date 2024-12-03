f6c2095ca9e14171d6504c46b1f60e8505da741c
Levente Nagy <nagyleventezsolt@gmail.com>
2024-12-03 17:33:14 +0100

    sync

--- git.sh
+++ git.sh
@@ -2,10 +2,8 @@ sudo chmod 777 -R report
 sudo chown levente:levente -R report
 sudo chmod 777 -R experiments
 sudo chown levente:levente -R experiments
-sudo chmod 777 -R source/data/native/models
-sudo chown levente:levente -R source/data/native/models
-sudo chmod 777 -R source/data/normalized/models
-sudo chown levente:levente -R source/data/normalized/models
+sudo chmod 777 -R source/data/models
+sudo chown levente:levente -R source/data/models
 git add -A
 git commit -m "$1"
 git push
